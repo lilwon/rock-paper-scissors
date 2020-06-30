499103ce507ae3fce701f15eb2ed69c5b1561ea9
Lillian Won <lwon1@uci.edu>
2020-06-30 00:04:27 -0700

    Changed text outputs to DOM methods

--- script.js
+++ script.js
@@ -22,31 +22,50 @@ function playRound() {
     const playerSelection = this.id;
     
     if ( playerSelection === "rock" ) {
-        if ( computerSelection === "paper" ) 
-            return console.log("You Lose! Paper beats Rock"); 
-        else if ( computerSelection === "scissors") 
-            return console.log("You Win! Rock beats Scissors"); 
-        else 
-            return console.log("You Tie! Rock against Rock"); 
+        if ( computerSelection === "paper" ) {
+            return document.getElementById("response").innerHTML
+                = "You Lose! Paper beats Rock"; 
+        }
+        else if ( computerSelection === "scissors") { 
+            return document.getElementById("response").innerHTML
+                = "You Win! Rock beats Scissors";
+        }
+        else { 
+            return document.getElementById("response").innerHTML
+                = "You Tie! Rock against Rock";
+        }
     }
     else if ( playerSelection === "paper" ) {
-        if ( computerSelection === "scissors" )
-            return console.log("You Lose! Scissors beats Paper"); 
-        else if ( computerSelection === "rock") 
-            return console.log("You Win! Paper beats Rock"); 
-        else 
-            return console.log("You Tie! Paper against Paper"); 
+        if ( computerSelection === "scissors" ) {
+            return document.getElementById("response").innerHTML
+                = "You Lose! Scissors beats Paper"; 
+        } 
+        else if ( computerSelection === "rock") { 
+            return document.getElementById("response").innerHTML
+                = "You Win! Paper beats Rock";
+        } 
+        else { 
+            return document.getElementById("response").innerHTML
+                = "You Tie! Paper against Paper";
+        }
     }
     else if ( playerSelection === "scissors") {
-        if ( computerSelection === "rock" ) 
-            return console.log("You Lose! Rock beats Scissors"); 
-        else if ( computerSelection === "paper") 
-            return console.log("You Win! Scissors beats Paper"); 
-        else 
-            return console.log("You Tie! Scissors against Scissors"); 
+        if ( computerSelection === "rock" ) { 
+            return document.getElementById("response").innerHTML 
+                = "You Lose! Rock beats Scissors";
+        } 
+        else if ( computerSelection === "paper") { 
+            return document.getElementById("response").innerHTML 
+                = "You Win! Scissors beats Paper";
+        }
+        else { 
+            return document.getElementById("response").innerHTML
+                = "You Tie! Scissors against Scissors";
+        } 
     }
     else {
-        return console.log("Not a correct value"); 
+        return document.getElementById("response").innerHTML 
+            = "Not a correct value"; 
     }
 }
 
@@ -79,7 +98,6 @@ function game() {
     }
     */
 }
-
 
 const rockBtn= document.querySelector('#rock');
 rockBtn.addEventListener('click', playRound);  
